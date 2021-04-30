7f3b940ae5135010f3a534ddecbf1bdca224a5c7
UtkarshKeerti <utkarsh.keerti@gmail.com>
2021-05-01 03:55:41 +0530

    1st commit

---
+++ main.js
@@ -0,0 +1,154 @@
+// var display = document.getElementsByClassName("dots-nav");
+// var i;
+// var a = 0;
+// for (i = 0; i <= display.length; i++) {
+//   display[i].addEventListener('click', function () {
+//     a++;
+//     if (a == 1) {
+//       console.log("1");
+//       $(".conatainer-7").removeClass("display-none");
+//     }
+//   });
+
+// }
+
+function display_fun1() {
+  console.log("1");
+  $(".container-6").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-5").addClass("display-none");
+  $("svg").style[background - color] = "#fff";
+}
+
+function display_fun2() {
+  console.log("1");
+  $(".container-7").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-7");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-6");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-6").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-5").addClass("display-none");
+}
+
+function display_fun3() {
+  console.log("1");
+  $(".container-1").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-6").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-5").addClass("display-none");
+}
+
+function display_fun4() {
+  console.log("1");
+  $(".container-2").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-6").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-5").addClass("display-none");
+}
+
+function display_fun5() {
+  console.log("1");
+  $(".container-3").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-6").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-5").addClass("display-none");
+}
+
+function display_fun6() {
+  console.log("1");
+  $(".container-4").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-6").addClass("display-none");
+  $(".container-5").addClass("display-none");
+}
+
+function display_fun7() {
+  console.log("1");
+  $(".container-5").removeClass("display-none");
+  $(".svg-bg").addClass("svg-bg-5");
+  $(".svg-bg").removeClass("svg-bg-2");
+  $(".svg-bg").removeClass("svg-bg-3");
+  $(".svg-bg").removeClass("svg-bg-4");
+  $(".svg-bg").removeClass("svg-bg-6");
+  $(".svg-bg").removeClass("svg-bg-1");
+  $(".svg-bg").removeClass("svg-bg-7");
+  // $(".landing").prevAll().addClass("display-none");
+  // $(".landing").nextAll().addClass("display-none");
+  $(".container-7").addClass("display-none");
+  $(".container-1").addClass("display-none");
+  $(".container-2").addClass("display-none");
+  $(".container-3").addClass("display-none");
+  $(".container-4").addClass("display-none");
+  $(".container-6").addClass("display-none");
+}
