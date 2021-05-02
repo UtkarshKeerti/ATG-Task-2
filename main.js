2dca2d72825f3a7d1891f2c9909e0157aec10d08
UtkarshKeerti <utkarsh.keerti@gmail.com>
2021-05-02 18:18:25 +0530

    finishing touches

--- main.js
+++ main.js
@@ -1,34 +1,8 @@
-// var display = document.getElementsByClassName("dots-nav");
-// var i;
-// var a = 0;
-// for (i = 0; i <= display.length; i++) {
-//   display[i].addEventListener('click', function () {
-//     a++;
-//     if (a == 1) {
-//       console.log("1");
-//       $(".conatainer-7").removeClass("display-none");
-//     }
-//   });
-
-// }
-
-// $(".largest-div").scroll(function () {
-//   if ($(this.scrollTop() > 200)) {
-//     console.log("1");
-//     display_fun2();
-//   }
-// })
-
 var tl = gsap.timeline({
   defaults: {
     duration: 0.7
   }
 });
-
-// ScrollTrigger.defaults({
-//   trigger: ".largest-div",
-//   toggleActions: "restart reset restart reset"
-// })
 
 function display_fun1() {
   tl.from(".c1-lp-0", {
@@ -55,7 +29,6 @@ function display_fun1() {
     duration: 1.3,
     y: 100
   });
-  console.log("1");
   $(".container-6").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -64,8 +37,6 @@ function display_fun1() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -100,7 +71,6 @@ function display_fun2() {
     duration: 1,
     y: 200
   });
-  console.log("1");
   $(".container-7").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-7");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -109,8 +79,6 @@ function display_fun2() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-6");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-6").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -144,7 +112,6 @@ function display_fun3() {
     duration: 1,
     y: 200
   });
-  console.log("1");
   $(".container-1").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -153,8 +120,6 @@ function display_fun3() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-6").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -203,7 +168,6 @@ function display_fun4() {
     duration: 1.3,
     y: -100
   });
-  console.log("1");
   $(".container-2").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-2");
   $(".svg-bg").removeClass("svg-bg-6");
@@ -212,8 +176,6 @@ function display_fun4() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-6").addClass("display-none");
@@ -262,7 +224,6 @@ function display_fun5() {
     duration: 1.3,
     y: -300
   });
-  console.log("1");
   $(".container-3").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-3");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -271,8 +232,6 @@ function display_fun5() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -301,7 +260,6 @@ function display_fun6() {
     duration: 1.3,
     y: 700
   });
-  console.log("1");
   $(".container-4").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-4");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -310,8 +268,6 @@ function display_fun6() {
   $(".svg-bg").removeClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -350,7 +306,6 @@ function display_fun7() {
     duration: 1.3,
     y: 200
   });
-  console.log("1");
   $(".container-5").removeClass("display-none");
   $(".svg-bg").addClass("svg-bg-5");
   $(".svg-bg").removeClass("svg-bg-2");
@@ -359,8 +314,6 @@ function display_fun7() {
   $(".svg-bg").removeClass("svg-bg-6");
   $(".svg-bg").removeClass("svg-bg-1");
   $(".svg-bg").removeClass("svg-bg-7");
-  // $(".landing").prevAll().addClass("display-none");
-  // $(".landing").nextAll().addClass("display-none");
   $(".container-7").addClass("display-none");
   $(".container-1").addClass("display-none");
   $(".container-2").addClass("display-none");
@@ -371,47 +324,22 @@ function display_fun7() {
 
 window.addEventListener('scroll', () => {
   var scrolled = window.pageYOffset;
-  console.log(scrolled);
-  // console.log(scrolled);
   if (scrolled === 0) {
-    console.log("!");
     display_fun1();
   } else if (scrolled === 100) {
-    console.log("!");
     display_fun2();
   } else if (scrolled === 200) {
-    console.log("!");
     display_fun3();
   } else if (scrolled === 300) {
-    console.log("!");
     display_fun4();
   } else if (scrolled === 400) {
-    console.log("!");
     display_fun5();
   } else if (scrolled === 500) {
-    console.log("!");
     display_fun6();
   } else if (scrolled === 600) {
-    console.log("!");
     display_fun7();
   }
 });
-
-// ScrollTrigger.defaults({
-//   trigger: ".largest-div",
-//   toggleActions: "restart reset restart reset",
-//   markers: true
-// })
-
-// var tl2 = gsap.timeline({
-//   scrollTrigger: {
-//     id: "slide-2",
-//     start: "center 85%",
-//     end: "center 70%"
-//   }
-// })
-
-// tl2.call(display_fun2);
 
 $('.owl-carousel').owlCarousel({
   loop: true,
